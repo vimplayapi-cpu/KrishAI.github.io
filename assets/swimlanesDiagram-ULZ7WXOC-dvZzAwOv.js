012033f4a432d4e112b63c2af1b2f522536699a4
vimplayapi-cpu <2.68168295e+08+vimplayapi-cpu@users.noreply.github.com>
2026-08-18 21:09:33 +0000

    Publish built frontend at GitHub Pages root

---
+++ assets/swimlanesDiagram-ULZ7WXOC-dvZzAwOv.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-UKHOOZJN-D0FwfwU-.js";import{_ as a}from"./mermaid.core-C9wWhtZ-.js";import"./chunk-5VM5RSS4-E97DiKcN.js";import"./chunk-XXDRQBXY-Dp-57XnY.js";import"./chunk-KBJHAD2P-Dig34APM.js";import"./chunk-2GRJ4B5K-CqDxuo26.js";import"./channel-BOE9eA-U.js";import"./index-B0ee3Njt.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
